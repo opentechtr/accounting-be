82014f50338bce79c240840ccbf6382d6c481b8f
UlasGultekin <gltknulas96@gmail.com>
2025-03-08 00:08:45 +0300

    example use

--- Accounting/src/main/java/com/otcp/accounting/product/dto/CreateCategoryDTO.java
+++ Accounting/src/main/java/com/otcp/accounting/product/dto/CreateCategoryDTO.java
@@ -1,4 +1,16 @@
 package com.otcp.accounting.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CreateCategoryDTO {
+    @NotBlank(message = "Category name is mandatory")
+    private String name;
+
+    @Size(max = 255, message = "Description can be up to 255 characters")
+    private String description;
 }
