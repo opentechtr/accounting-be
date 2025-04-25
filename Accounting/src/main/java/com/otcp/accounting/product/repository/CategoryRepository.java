200adc4dd3f5883db810721e93054697b37a9cde
cengzayhn <mcengzayhn@gmail.com>
2025-04-26 00:03:07 +0300

    added query to check whether name exists or not

--- Accounting/src/main/java/com/otcp/accounting/product/repository/CategoryRepository.java
+++ Accounting/src/main/java/com/otcp/accounting/product/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.otcp.accounting.common.base.BaseRepository;
 import com.otcp.accounting.product.entity.Category;
 
 public interface CategoryRepository extends BaseRepository<Category, Long> {
+    boolean existsByName(String name);
 }
