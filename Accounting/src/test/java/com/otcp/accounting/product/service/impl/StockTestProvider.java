43d65c6c3e6c9d56f50a115753edd5dc692ce743
cengzayhn <mcengzayhn@gmail.com>
2025-05-01 21:34:12 +0300

    test classes are created for StockServiceImplTest and added tests for updateStock method

---
+++ Accounting/src/test/java/com/otcp/accounting/product/service/impl/StockTestProvider.java
@@ -0,0 +1,35 @@
+package com.otcp.accounting.product.service.impl;
+
+import com.otcp.accounting.common.base.EntityStatus;
+import com.otcp.accounting.product.entity.Product;
+import com.otcp.accounting.product.entity.Stock;
+import com.otcp.accounting.product.entity.Warehouse;
+
+public class StockTestProvider {
+
+    public static Stock getStock() {
+        Stock stock = new Stock();
+        stock.setId(1L);
+        stock.setQuantity(100);
+        stock.setEntityStatus(EntityStatus.ACTIVE);
+        stock.setProduct(getProduct());
+        stock.setWarehouse(getWarehouse());
+        return stock;
+    }
+
+    public static Product getProduct() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Test Product");
+        product.setCode("P123");
+        return product;
+    }
+
+    public static Warehouse getWarehouse() {
+        Warehouse warehouse = new Warehouse();
+        warehouse.setId(1L);
+        warehouse.setName("Main Warehouse");
+        warehouse.setLocation("Istanbul");
+        return warehouse;
+    }
+}
